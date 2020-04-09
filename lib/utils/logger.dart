023716daf7c31e93191253f0f15d339ebb41a292
leiming <2008weileiming@gmail.com>
2020-04-09 11:17:32 +0800

    refactor: _logger

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,44 +1,44 @@
 import 'package:logger/logger.dart' as VLogger;
 
-var logger = VLogger.Logger(
-  printer: VLogger.PrettyPrinter(
-    methodCount: 2,
-    errorMethodCount: 8,
-    lineLength: 120,
-    colors: false,
-    printEmojis: true,
-    printTime: true,
-  ),
-);
-
 class Logger {
+  static var _logger = VLogger.Logger(
+    printer: VLogger.PrettyPrinter(
+      methodCount: 2,
+      errorMethodCount: 8,
+      lineLength: 120,
+      colors: false,
+      printEmojis: true,
+      printTime: true,
+    ),
+  );
+
   /// Log a message at level [Level.verbose].
   static void verbose(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    logger.v(message, error, stackTrace);
+    _logger.v(message, error, stackTrace);
   }
 
   /// Log a message at level [Level.debug].
   static void debug(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    logger.d(message, error, stackTrace);
+    _logger.d(message, error, stackTrace);
   }
 
   /// Log a message at level [Level.info].
   static void info(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    logger.i(message, error, stackTrace);
+    _logger.i(message, error, stackTrace);
   }
 
   /// Log a message at level [Level.warning].
   static void warning(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    logger.w(message, error, stackTrace);
+    _logger.w(message, error, stackTrace);
   }
 
   /// Log a message at level [Level.error].
   static void error(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    logger.e(message, error, stackTrace);
+    _logger.e(message, error, stackTrace);
   }
 
   /// Log a message at level [Level.wtf].
   static void wtf(dynamic message, [dynamic error, StackTrace stackTrace]) {
-    logger.wtf(message, error, stackTrace);
+    _logger.wtf(message, error, stackTrace);
   }
 }
