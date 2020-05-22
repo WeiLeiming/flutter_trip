a9818948795b8aa62b9c0072ab30447e682a7e71
leiming <2008weileiming@gmail.com>
2020-05-22 15:35:57 +0800

    feat: search list

--- lib/dao/search_dao.dart
+++ lib/dao/search_dao.dart
@@ -5,12 +5,14 @@ import 'package:trip/model/search_model.dart';
 
 /// 搜索接口
 class SearchDao {
-  static Future<SearchModel> fetch(String url) async {
+  static Future<SearchModel> fetch(String url, String text) async {
     final response = await http.get(url);
     if (response.statusCode == 200) {
       Utf8Decoder utf8decoder = Utf8Decoder();
       var result = json.decode(utf8decoder.convert(response.bodyBytes));
-      return SearchModel.fromJson(result);
+      SearchModel model = SearchModel.fromJson(result);
+      model.keyword = text;
+      return model;
     } else {
       throw Exception('Failed to load search_page.json');
     }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -3,46 +3,104 @@ import 'package:trip/dao/search_dao.dart';
 import 'package:trip/model/search_model.dart';
 import 'package:trip/widget/search_bar.dart';
 
+const URL =
+    'https://m.ctrip.com/restapi/h5api/globalsearch/search?source=mobileweb&action=mobileweb&keyword=';
+
 class SearchPage extends StatefulWidget {
+  final bool hideLeft;
+  final String searchUrl;
+  final String keyword;
+  final String hint;
+
+  const SearchPage(
+      {Key key, this.hideLeft, this.searchUrl = URL, this.keyword, this.hint})
+      : super(key: key);
+
   @override
   _SearchPageState createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
-  String showText = '';
+  SearchModel searchModel;
+  String keyword;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Column(
         children: <Widget>[
-          SearchBar(
-            hideLeft: true,
-            defaultText: '哈哈',
-            hint: '123',
-            leftButtonClick: () {
-              Navigator.pop(context);
-            },
-            onChanged: _onTextChange,
+          _appBar(),
+          MediaQuery.removePadding(
+            context: context,
+            removeTop: true,
+            child: Expanded(
+              flex: 1,
+              child: ListView.builder(
+                  itemCount: searchModel?.data?.length ?? 0,
+                  itemBuilder: (BuildContext context, int position) {
+                    return _item(position);
+                  }),
+            ),
           ),
-          InkWell(
-            onTap: () {
-              SearchDao.fetch(
-                      'https://m.ctrip.com/restapi/h5api/globalsearch/search?source=mobileweb&action=mobileweb&keyword=长城')
-                  .then((SearchModel value) {
-                setState(() {
-                  showText = value.data[0].url;
-                });
-              });
-            },
-            child: Text('Get'),
-          ),
-          Text(showText),
         ],
       ),
     );
   }
 
-  _onTextChange(String text) {}
+  _onTextChange(String text) {
+    keyword = text;
+    if (text.length == 0) {
+      setState(() {
+        searchModel = null;
+      });
+      return;
+    }
+    String url = widget.searchUrl + text;
+    SearchDao.fetch(url, text).then((SearchModel model) {
+      if (model.keyword == keyword) {
+        setState(() {
+          searchModel = model;
+        });
+      }
+    }).catchError((e) {
+      print(e);
+    });
+  }
+
+  _appBar() {
+    return Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Color(0x66000000), Colors.transparent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          )),
+          child: Container(
+            padding: EdgeInsets.only(top: 20),
+            height: 80,
+            decoration: BoxDecoration(color: Colors.white),
+            child: SearchBar(
+              hideLeft: widget.hideLeft,
+              defaultText: widget.keyword,
+              hint: widget.hint,
+              leftButtonClick: () {
+                Navigator.pop(context);
+              },
+              onChanged: _onTextChange,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  _item(int position) {
+    if (searchModel == null || searchModel.data == null) return null;
+    SearchItem item = searchModel.data[position];
+    return Text(
+      item.word,
+    );
+  }
 }
